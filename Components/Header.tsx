f7b88726eb821d9e5ea0422410fd284d43138643
un-earthly <vijayalamin@gmail.com>
2023-09-04 11:03:17 -0700

    layout n bg

---
+++ Components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+export default function Header() {
+  return (
+      <div className=''>
+          {/* Banner */}
+          <div className='bg-[url("https://whatarestaurant.com/uploads/restorants/a49fdbcb-3fe1-46a4-a37b-af7c0bf74579_cover.jpg")] bg-cover bg-center h-[28rem]'>
+              
+          </div>
+          <Navbar />
+    </div>
+  )
+}
